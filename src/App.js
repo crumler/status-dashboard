0412e851b780b21ba4e66eda02129bb7d5470f5e
Christopher Rumler <crumler@dminc.com>
2021-10-20 16:40:27 -0400

    Axios fetch completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const HerokuURL = "https://status.heroku.com/api/v4/current-status";
+
+  let [herokuStatus, setHerokuStatus] = useState("");
+
+const fetchData = () => {
+  Axios.get(HerokuURL).then((response) => {
+    console.log(response.data.status[0].status); //this returns 'green' in the console
+    setHerokuStatus = (response.data.status[0].status);
+  });
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>EDAT Operations Status Page Dashboard</h1>
+      <div className="button">
+        <button onClick={fetchData}>Fetch!</button>
+      </div>
+      <div className="currentStatus">
+        <h2>{herokuStatus}</h2>
+      </div>
+    </header>
+
   );
 }
 
---
+++ src/components/HerokuStatus.js
@@ -0,0 +1,6 @@
+
+const HerokuStatus = () => {
+
+};
+
+export default HerokuStatus;
